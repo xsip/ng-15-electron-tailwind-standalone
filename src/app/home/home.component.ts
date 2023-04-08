f13fae4d903acf2af1ebc8d58b3a79f64c71c9e1
Kerschbaumer Stefan <xsip@pm.me>
2023-04-08 07:27:20 +0200

    adds sample form

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,18 +1,32 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {FormBuilder, Validators} from '@angular/forms';
+import {FormComponent, MetaData} from '../ui/form/form.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormComponent],
   template: `
-    <p class="bg-slate-800 text-white">
-      home works!
-    </p>
+      <app-form [fg]="fg" [metaData]="metaData"></app-form>
   `,
   styles: [
   ]
 })
 export class HomeComponent {
-
+  fb = inject(FormBuilder);
+  fg = this.fb.group({
+    firstName: ['', Validators.required],
+    lastName: ['', Validators.required],
+  });
+  metaData: MetaData<typeof this.fg.value> = {
+    firstName: {
+      placeholder: 'Firstname',
+      label: 'Firstname'
+    },
+    lastName: {
+      placeholder: 'Lastname',
+      label: 'Lastname'
+    }
+  }
 }
---
+++ src/app/ui/form/form.component.ts
@@ -0,0 +1,30 @@
+import {Component, Input} from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {FormGroup, ReactiveFormsModule} from '@angular/forms';
+
+export type MetaData<T> = Record<keyof T,  {
+  label: string;
+  placeholder?: string;
+}>
+@Component({
+  selector: 'app-form',
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
+  template: `
+    <form [formGroup]="fg">
+      <div class="grid gap-6 mb-6 md:grid-cols-3">
+        <div *ngFor="let key of keys(metaData)">
+          <label for="{{key}}" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">{{metaData[key].label}}</label>
+          <input type="text" id="{{key}}" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-slate-500 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="{{metaData[key].placeholder}}">
+        </div>
+      </div>
+    </form>
+  `,
+  styles: [
+  ]
+})
+export class FormComponent {
+  @Input() fg!: FormGroup;
+  @Input() metaData!: MetaData<any>;
+  keys = Object.keys;
+}
